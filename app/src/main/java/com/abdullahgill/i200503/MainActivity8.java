9c661ae85765ef3ab562e1f8eda6e114722497f2
Gill <abdullahtariqgill@gmail.com>
2023-09-15 14:56:53 +0500

    Assignment #1 Screen#8 and other supporting files

---
+++ app/src/main/java/com/abdullahgill/i200503/MainActivity8.java
@@ -0,0 +1,22 @@
+package com.abdullahgill.i200503;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+
+public class MainActivity8 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_8);
+    }
+
+    // Enable the Up button
+
+
+
+
+}
