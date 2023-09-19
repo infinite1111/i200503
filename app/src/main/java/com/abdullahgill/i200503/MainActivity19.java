68e31013fcdb68400033eee19f2b76058a9f0fa6
Gill <abdullahtariqgill@gmail.com>
2023-09-19 14:53:04 +0500

    Assignment #1 screens

---
+++ app/src/main/java/com/abdullahgill/i200503/MainActivity19.java
@@ -0,0 +1,22 @@
+package com.abdullahgill.i200503;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+
+public class MainActivity19 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_19);
+    }
+
+    // Enable the Up button
+
+
+
+
+}
